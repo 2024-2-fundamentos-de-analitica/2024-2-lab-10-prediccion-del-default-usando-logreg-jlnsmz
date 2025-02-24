8720b481d0cf86f8437673a5921ebcc8bdd69097
Julián Orrego Martínez <jorrego@unal.edu.co>
2025-02-23 21:42:22 -0500

    intento 1

--- homework/homework.py
+++ homework/homework.py
@@ -95,3 +95,140 @@
 # {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
 # {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
 #
+
+import pandas as pd
+import gzip
+import pickle
+import json
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import os
+
+
+def limpieza(df):
+    # Paso 1.
+    df = df.copy()
+    df.rename(columns={'default payment next month': 'default'}, inplace=True)
+    df.drop('ID', axis=1, inplace=True)
+    df.dropna(inplace=True)
+    df = df[(df["EDUCATION"]!=0) & (df["MARRIAGE"]!=0)]
+    df["EDUCATION"] = df["EDUCATION"].apply(lambda x: 4 if x>4 else x) 
+    return df
+
+def crear_pipeline(categorical_features, k_features=10):
+    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('cat', categorical_transformer, categorical_features),
+        ], 
+        remainder='passthrough' 
+    )
+
+    pipeline = Pipeline(steps=[
+        ('preprocessor', preprocessor),
+        ('scaler', MinMaxScaler()),
+        ('selector', SelectKBest(score_func=f_classif, k=k_features)),
+        ('classifier', LogisticRegression(max_iter=500, random_state=42))
+    ])
+    
+    return pipeline
+def optimizar_hiperparametros(pipeline, x_train, y_train):
+
+    param_grid = {
+        'selector__k': range(1, 11),
+        'classifier__C': [0.001, 0.01, 0.1, 1, 10, 100],
+        'classifier__penalty':['l1','l2'],
+        'classifier__solver':['liblinear'],
+        "classifier__max_iter": [100,200],
+    }
+
+
+    grid_search = GridSearchCV(
+        estimator=pipeline,          
+        param_grid=param_grid,        
+        cv=10,                        
+        scoring='balanced_accuracy',
+        n_jobs=-1,
+        refit=True                    
+    )
+
+    
+    return grid_search
+
+def calcular_metricas(y_true, y_pred, dataset):
+    return {
+        'type': 'metrics',
+        'dataset': dataset,
+        'precision': precision_score(y_true, y_pred),
+        'balanced_accuracy': balanced_accuracy_score(y_true, y_pred),
+        'recall': recall_score(y_true, y_pred),
+        'f1_score': f1_score(y_true, y_pred)
+    }
+def calcular_matriz_confusion(y_true, y_pred, dataset):
+    cm = confusion_matrix(y_true, y_pred)
+    return {
+        'type': 'cm_matrix',
+        'dataset': dataset,
+        'true_0': {"predicted_0": int(cm[0, 0]), "predicted_1": int(cm[0, 1])},
+        'true_1': {"predicted_0": int(cm[1, 0]), "predicted_1": int(cm[1, 1])}
+    }
+
+def save_model(path: str, estimator: GridSearchCV):
+    with gzip.open(path, 'wb') as f:
+        pickle.dump(estimator, f)
+def main():
+    # Cargar los datasets
+    test_data = pd.read_csv("files/input/test_data.csv.zip", compression="zip")
+    train_data = pd.read_csv("files/input/train_data.csv.zip", compression="zip")
+
+    # Limpieza
+    test_data=limpieza(test_data)
+    train_data=limpieza(train_data)
+
+    x_train=train_data.drop('default', axis=1)
+    y_train=train_data['default']
+    x_test=test_data.drop('default', axis=1)
+    y_test=test_data['default']
+
+    # Identificar variables categóricas
+    categorical_features = x_train.select_dtypes(include=['object', 'category']).columns.tolist()
+
+    # Crear y entrenar el pipeline
+    pipeline = crear_pipeline(categorical_features)
+
+    # Optimización de hiperparámetros usando validación cruzada
+    grid_search = optimizar_hiperparametros(pipeline, x_train, y_train)
+    # Ajustar el modelo con la mejor combinación de hiperparámetros
+    grid_search.fit(x_train, y_train)
+
+
+    path2 = "./files/models/"
+    save_model(
+        os.path.join(path2, 'model.pkl.gz'),
+        grid_search,
+    )
+   
+
+    pred_train = grid_search.predict(x_train)
+    pred_test = grid_search.predict(x_test)
+
+    metrics = [
+        calcular_metricas(y_train, pred_train, 'train'),
+        calcular_metricas(y_test, pred_test, 'test'),
+        calcular_matriz_confusion(y_train, pred_train, 'train'),
+        calcular_matriz_confusion(y_test, pred_test, 'test')
+    ]
+    with open("files/output/metrics.json", "w") as f:
+        for metric in metrics:
+            f.write(json.dumps(metric) + "\n")
+   
+
+if __name__ == "__main__":
+    main()
